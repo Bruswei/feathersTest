34853b5fedf76c4d803d79d8f75776660d90046d
Yijun Pan Stautland <yijun@staut.land>
2017-07-08 16:56:24 +0200

    change tslint with single quoates

--- Todo-ang/src/Todo/todo.component.ts
+++ Todo-ang/src/Todo/todo.component.ts
@@ -2,7 +2,7 @@
  * Created by yijun on 07.07.17.
  */
 
-import {Component} from '@angular/core';
+import {Component} from "@angular/core";
 @Component ({
   selector: 'todo-component',
   templateUrl: './todo.component.html',
